5627597338320c42c9007434e9fe618386564541
akash <aakashkr787029@gmail.com>
2020-10-02 19:19:34 +0530

    "version 2.0"

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("abhishek")
+import time
+name = input("enter your name ")
+text = ["Hello", "How are you"]
+for i in range(len(text)):
+    print(f"{text[i]} {name}")
+    time.sleep(1)
+print("I am bot try app in python")
